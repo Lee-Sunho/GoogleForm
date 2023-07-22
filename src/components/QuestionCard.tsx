05e5c6c51bf908ea99731779cad3ce79d7d341c6
Lee-Sunho <sunho9889@naver.com>
2023-07-22 20:12:38 +0900

    feat: Form questionList 렌더링 구현

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,96 @@
-const QuestionCard = () => {
-  return null;
+import styled from "styled-components";
+import { QuestionProps } from "../redux/modules/questionSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../redux/configureStore";
+import { setFocus } from "../redux/modules/focusSlice";
+import { setQuestionTitle } from "../redux/modules/questionSlice";
+import SelectType from "./SelectType";
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 768px;
+  position: relative;
+  border: 1px solid ${(props) => props.theme.bordergray};
+  border-radius: 8px;
+  background-color: white;
+  margin-top: 13px;
+`;
+
+const ContentWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  padding: 24px;
+`;
+
+const FocusLine = styled.div<{ focused: boolean }>`
+  position: absolute;
+  left: 0px;
+  background-color: ${(props) => props.theme.blue};
+  width: 6px;
+  height: 100%;
+  border-bottom-left-radius: 8px;
+  border-top-left-radius: 8px;
+  display: ${(props) => (props.focused ? "block" : "none")};
+`;
+
+const TitleWrapper = styled.div`
+  width: 100%;
+  padding: 8px, 24px;
+  margin-right: 24px;
+`;
+
+const TitleInput = styled.input<{ focused: boolean }>`
+  width: 100%;
+  font-size: 14px;
+  border: 0;
+  outline: none;
+  border-bottom: ${(props) =>
+    props.focused ? `1px solid ${props.theme.bordergray}` : "none"};
+  background-color: ${(props) =>
+    props.focused ? `${props.theme.lightgray}` : "transparent"};
+  padding: ${(props) => (props.focused ? "16px" : "0px")};
+  line-height: 135%;
+  &:focus {
+    border-bottom: 2px solid ${(props) => props.theme.darkpurple};
+  }
+`;
+
+const QuestionCard = ({
+  id,
+  questionTitle,
+  questionType,
+  contents,
+  isRequired,
+}: QuestionProps) => {
+  const dispatch = useDispatch();
+  const focusId = useSelector<RootState, string>((state) => {
+    return state.focus.focus;
+  });
+
+  const handleClick = () => {
+    dispatch(setFocus(id));
+  };
+
+  const handleTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setQuestionTitle({ id: id, questionTitle: e.target.value }));
+  };
+
+  return (
+    <Wrapper onClick={handleClick}>
+      <FocusLine focused={focusId === id ? true : false} />
+      <ContentWrapper>
+        <TitleWrapper>
+          <TitleInput
+            onChange={handleTitle}
+            focused={focusId === id ? true : false}
+            value={questionTitle}
+          />
+        </TitleWrapper>
+        <SelectType id={id} />
+      </ContentWrapper>
+    </Wrapper>
+  );
 };
 
 export default QuestionCard;
--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,15 +1,38 @@
 import TitleCard from "../components/TitleCard";
 import styled from "styled-components";
+import BtnAddQuestion from "../components/BtnAddQuestion";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/configureStore";
+import { QuestionProps } from "../redux/modules/questionSlice";
+import QuestionCard from "../components/QuestionCard";
 
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
 `;
 
+const CardList = styled.div``;
+
 const Form = () => {
+  const questionList = useSelector<RootState, QuestionProps[]>((state) => {
+    return state.question;
+  });
   return (
     <Wrapper>
-      <TitleCard />
+      <CardList>
+        <TitleCard />
+        {questionList.map((card) => (
+          <QuestionCard
+            key={card.id}
+            id={card.id}
+            questionTitle={card.questionTitle}
+            questionType={card.questionType}
+            contents={card.contents}
+            isRequired={card.isRequired}
+          />
+        ))}
+      </CardList>
+      <BtnAddQuestion />
     </Wrapper>
   );
 };
