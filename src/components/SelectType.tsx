135c1dba666e3fa0def70337e04f343279ecb99e
Lee-Sunho <sunho9889@naver.com>
2023-07-22 22:14:06 +0900

    design: menuitem에 아이콘 추가

--- src/components/SelectType.tsx
+++ src/components/SelectType.tsx
@@ -1,17 +1,31 @@
 import {
-  Box,
-  InputLabel,
   MenuItem,
   FormControl,
   Select,
   SelectChangeEvent,
 } from "@mui/material";
 import styled from "styled-components";
-import { useState } from "react";
 import { QuestionProps, QuestionTypes } from "../redux/modules/questionSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/configureStore";
 import { setQuestionType } from "../redux/modules/questionSlice";
+import Icon_shortText from "@mui/icons-material/ShortTextOutlined";
+import Icon_longText from "@mui/icons-material/SubjectOutlined";
+import Icon_radio from "@mui/icons-material/RadioButtonCheckedOutlined";
+import Icon_checkbox from "@mui/icons-material/CheckBoxOutlined";
+import Icon_dropdown from "@mui/icons-material/ArrowDropDownCircleOutlined";
+
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  color: ${(props) => props.theme.icongray};
+`;
+
+const Text = styled.span`
+  font-size: 12px;
+  text-align: center;
+  margin-left: 5px;
+`;
 
 interface IProps {
   id: string;
@@ -28,18 +42,50 @@ const SelectType = ({ id }: IProps) => {
     dispatch(setQuestionType({ id: id, questionType: e.target.value }));
   };
   return (
-    <FormControl sx={{ m: 1, width: 208, height: 48 }} size="small">
+    <FormControl
+      sx={{
+        m: 1,
+        width: 208,
+        height: 4,
+      }}
+      size="small"
+    >
       <Select
         onChange={handleSelect}
         value={current?.questionType}
         labelId="demo-simple-select-label"
         id="demo-simple-select"
       >
-        <MenuItem value={QuestionTypes.SHORTTEXT}>단답형</MenuItem>
-        <MenuItem value={QuestionTypes.LONGTEXT}>장문형</MenuItem>
-        <MenuItem value={QuestionTypes.RADIO}>객관식 질문</MenuItem>
-        <MenuItem value={QuestionTypes.CHECKBOX}>체크박스</MenuItem>
-        <MenuItem value={QuestionTypes.DROPDOWN}>드롭다운</MenuItem>
+        <MenuItem value={QuestionTypes.SHORTTEXT}>
+          <Item>
+            <Icon_shortText />
+            <Text>단답형</Text>
+          </Item>
+        </MenuItem>
+        <MenuItem value={QuestionTypes.LONGTEXT}>
+          <Item>
+            <Icon_longText />
+            <Text>장문형</Text>
+          </Item>
+        </MenuItem>
+        <MenuItem value={QuestionTypes.RADIO}>
+          <Item>
+            <Icon_radio />
+            <Text>객관식 질문</Text>
+          </Item>
+        </MenuItem>
+        <MenuItem value={QuestionTypes.CHECKBOX}>
+          <Item>
+            <Icon_checkbox />
+            <Text>체크박스</Text>
+          </Item>
+        </MenuItem>
+        <MenuItem value={QuestionTypes.DROPDOWN}>
+          <Item>
+            <Icon_dropdown />
+            <Text>드롭다운</Text>
+          </Item>
+        </MenuItem>
       </Select>
     </FormControl>
   );
