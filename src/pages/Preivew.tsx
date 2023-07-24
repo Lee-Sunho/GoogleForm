19e141f2460e9f0dda11394e25af6136ef403fc2
Lee-Sunho <sunho9889@naver.com>
2023-07-24 23:05:23 +0900

    feat: 필수입력 검사 기능 구현

--- src/pages/Preivew.tsx
+++ src/pages/Preivew.tsx
@@ -4,6 +4,8 @@ import { RootState } from "../redux/configureStore";
 import { QuestionProps, clearAnswer } from "../redux/modules/questionSlice";
 import TitleCard from "../components/TitleCard";
 import QuestionCard from "../components/QuestionCard";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Wrapper = styled.div`
   display: flex;
@@ -22,6 +24,7 @@ const ButtonArea = styled.div`
   display: flex;
   margin-top: 10px;
   margin-bottom: 10px;
+  padding: 0px 8px 0px 8px;
   width: 530px;
   justify-content: space-between;
   align-items: center;
@@ -44,15 +47,70 @@ const BtnClear = styled.button`
   padding: 6px;
 `;
 
+const RequiredArea = styled.div`
+  width: 530px;
+  padding: 8px;
+  font-size: 12px;
+  color: red;
+`;
+
 const Preview = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const questionList = useSelector<RootState, QuestionProps[]>((state) => {
     return state.question;
   });
 
+  const [inValidQuestions, setInvalidQuestions] = useState<string[]>([]);
+
+  const requiredList = questionList.filter((card) => card.isRequired === true);
+
   const handleClear = () => {
     dispatch(clearAnswer({}));
     window.location.reload();
+  };
+
+  useEffect(() => {
+    const func = async () => {
+      try {
+        await dispatch(clearAnswer({}));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    func();
+    checkRequired();
+  }, []);
+
+  const checkRequired = () => {
+    const temp: string[] = [];
+    for (let i = 0; i < requiredList.length; i++) {
+      const card = requiredList[i];
+      let isFulfill = false;
+      for (let j = 0; j < card.contents.length; j++) {
+        const item = card.contents[j];
+        if (item.isAnswer === true) {
+          isFulfill = true;
+          break;
+        }
+      }
+      if (!isFulfill) {
+        temp.push(card.id);
+      }
+    }
+    setInvalidQuestions(temp);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      await checkRequired();
+    } catch (err) {
+      console.log(err);
+    }
+    console.log(inValidQuestions.length);
+    if (inValidQuestions.length === 0) {
+      navigate("/preview/result");
+    }
   };
 
   return (
@@ -70,8 +128,13 @@ const Preview = () => {
           ))}
         </CardList>
       </CardListWrapper>
+      <RequiredArea>
+        {inValidQuestions.length > 0 ? (
+          <span>필수 질문들에 답해주세요</span>
+        ) : null}
+      </RequiredArea>
       <ButtonArea>
-        <BtnSubmit>제출</BtnSubmit>
+        <BtnSubmit onClick={handleSubmit}>제출</BtnSubmit>
         <BtnClear onClick={handleClear}>양식 지우기</BtnClear>
       </ButtonArea>
     </Wrapper>
